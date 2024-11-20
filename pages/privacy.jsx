0a79fd27dc5bf7b99b623dc7e2641dd317318b6f
Timmy <t.cavanagh2k1@gmail.com>
2024-11-20 17:18:48 -0500

    PP update
    Removed some lines made better

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,37 +5,37 @@ export default function Privacy() {
             <img width="500" src="/img/pp.png" />
             <p className="text-sm text-white/30 mb-4">Last updated: November 20, 2024  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ This Privacy Policy governs the collection, use, and sharing of personal information by CubeCloud, a Discord bot developed by Timmycas & Zaydocs. By using CubeCloud, you agree to the terms of this Privacy Policy. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>This Privacy Policy governs the collection, use, and sharing of personal information by CubeCloud, a Discord bot developed by Timmycas & Zaydocs. By using CubeCloud, you agree to the terms of this Privacy Policy. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
-        <p>⊳ Information We Collect:</p>
+        <p>Information We Collect:</p>
         <div className="py-3"></div>
         <p>⊳ We collect information that you provide to us through your use of the bot, such as your Discord user ID and username, server and channel information, and message content. We may also collect usage data, such as the frequency and duration of your use of the bot.</p>
         <div className="py-3"></div>
-        <p>⊳ How We Use Your Information:</p>   
+        <p>How We Use Your Information:</p>   
         <div className="py-3"></div>
         <p>⊳ We use your information to operate and improve CubeCloud, including to provide support and respond to your requests. We may also use your information to develop new features or services, to conduct research and analytics, and to comply with legal obligations.</p>
         <div className="py-3"></div>
-        <p>⊳ Sharing Your Information:</p>
+        <p>Sharing Your Information:</p>
         <div className="py-3"></div>
         <p>⊳ We do not sell or share your personal information with third parties. However, we may disclose your information in response to legal process or a request from a law enforcement agency or regulatory authority.</p> 
         <div className="py-3"></div>
-        <p>⊳ Data Retention:</p> 
+        <p>Data Retention:</p> 
         <div className="py-3"></div>
         <p>⊳ We retain your information for as long as necessary to provide CubeCloud’s services or as required by law. We will delete your information upon your request or when it is no longer needed.</p> 
         <div className="py-3"></div>
-        <p>⊳ Data Security:</p> 
+        <p>Data Security:</p> 
         <div className="py-3"></div>
         <p>⊳ We take reasonable measures to protect your information from unauthorized access, alteration, or destruction. However, no security measure is perfect, and we cannot guarantee the security of your information.</p> 
         <div className="py-3"></div>
-        <p>⊳ Changes to this Policy:</p> 
+        <p>Changes to this Policy:</p> 
         <div className="py-3"></div>
         <p>⊳ We may update this Privacy Policy from time to time, and we will post the updated policy on our website. Your continued use of CubeCloud after we make changes to this policy indicates your acceptance of the revised policy.</p> 
         <div className="py-3"></div>
-        <p>⊳ Contact Us:</p> 
+        <p>Contact Us:</p> 
         <div className="py-3"></div>
         <p>⊳ If you have any questions or concerns about this Privacy Policy, please join the <a className="text-white" href="https://discord.gg/m4RBuRUuhB">discord</a>.</p> 
         <div className="py-3"></div>
-        <p>⊳ Effective Date:</p> 
+        <p>Effective Date:</p> 
         <div className="py-3"></div>
         <p>⊳ This Privacy Policy is effective as of November 20, 2024.</p> 
     
