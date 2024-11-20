cfd07e1e022af78c26113e31b1de4096bbb6108d
Timmy <t.cavanagh2k1@gmail.com>
2024-11-20 15:09:13 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,7 +90,7 @@ Vote CubeCloud</a>
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with CubeCloud, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://share.cubecloud.ca/299729731237052417/_X0qa1.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
