a6b11228195cba9130bcca18745f12bb76ebe76a
Timmy <t.cavanagh2k1@gmail.com>
2024-11-20 16:10:22 -0500

    Fixed CubeCloud

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -152,7 +152,7 @@ Support Server</a>
   
   <Link href={"https://top.gg/bot/1202693084090081320"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote MusicMaker</a>
+Vote CubeCloud</a>
   </Link>
 </div>
 
