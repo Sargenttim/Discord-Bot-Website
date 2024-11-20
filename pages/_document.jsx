1a3e2b8922bfa69f89db6b3ec05fd5e191fd33f5
Timmy <t.cavanagh2k1@gmail.com>
2024-11-20 14:51:23 -0500

    Updated document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,11 +14,11 @@ class MyDocument extends Document {
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@CubeCloud" />
+          <meta name="twitter:creator" content="@CubeCloud" />
+          <meta property="og:url" content="https://www.cubecloud.ca" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="CubeCloud" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -29,11 +29,11 @@ class MyDocument extends Document {
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="CubeCloud" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="CubeCloud"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
