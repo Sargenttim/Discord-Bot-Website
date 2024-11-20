1a7ae9c6626219eff7a68cd406132ea0ca4e80a3
Timmy <t.cavanagh2k1@gmail.com>
2024-11-20 17:31:13 -0500

    Added new partner
    Added Nifty Servers

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -63,6 +63,22 @@ export default function Partners() {
                     </a>
                     </div>
 
+                    <div className="flex flex-col justify-center text-white rounded">
+            <a href="https://discord.gg/FFQKX7RSct">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://share.cubecloud.ca/299729731237052417/SUljtf.jpg" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Nifty Servers</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                      Simple Modded Rust Server PVE Come join to have some fun.
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
                 </div>
                 <div className="py-10"></div>
         </>
