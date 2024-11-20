3f93af7bc773971b3561e26bab40d122cfed0199
Timmy <t.cavanagh2k1@gmail.com>
2024-11-20 17:08:56 -0500

    Deleted Error

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,12 +1,4 @@
-import React from 'react';
-import logo from './tos.png';
-console.log(logo); // /logo.84287d09.png
-
-function Header() {
-  // Import result is the URL of your image
-  return <img src={logo} alt="Logo" />;
-}
-  
+ 
 export default function Tos() {
   
   return (
