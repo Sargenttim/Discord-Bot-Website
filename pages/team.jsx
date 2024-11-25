fa5c65d3a0371a089d44776fb85ad647c8ac8494
Timmy <t.cavanagh2k1@gmail.com>
2024-11-25 11:15:03 -0500

    Update team.jsx
    photo update test

--- pages/team.jsx
+++ pages/team.jsx
@@ -17,7 +17,7 @@ export default function Teams() {
 
             <div className="flex flex-col justify-center text-white rounded">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://share.cubecloud.ca/299729731237052417/E2XSMW.gif" />
+                            <img className="rounded-full h-32 w-32" src="/img/tos.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Founder/CEO Timmy</h1>        
                                 <div className="flex items-center mt-1">
