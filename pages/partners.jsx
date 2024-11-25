4f5dc1bdd653a41f4f1131cfb1d053f6c8cc3f90
Timmy <t.cavanagh2k1@gmail.com>
2024-11-25 11:28:40 -0500

    Added new photos  and fixed partners

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,7 +18,7 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://valornode.net">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://share.cubecloud.ca/299729731237052417/em3Fyb.png" />
+                            <img className="rounded-full h-32 w-32" src="/partnerimg/valornode.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Valor Node</h1>        
                                 <div className="flex items-center mt-1">
@@ -34,7 +34,7 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://store.maverickhq.ca/">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://share.cubecloud.ca/299729731237052417/MY-iDs.png" />
+                            <img className="rounded-full h-32 w-32" src="/partnerimg/maverickhq.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Maverick HQ</h1>        
                                 <div className="flex items-center mt-1">
@@ -50,7 +50,7 @@ export default function Partners() {
                     <div className="flex flex-col justify-center text-white rounded">
             <a href="https://toxicityprotection.com/">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://share.cubecloud.ca/299729731237052417/w3IFqP.webp" />
+                            <img className="rounded-full h-32 w-32" src="/partnerimg/tp.webp" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Toxicity Protection</h1>        
                                 <div className="flex items-center mt-1">
@@ -66,7 +66,7 @@ export default function Partners() {
                     <div className="flex flex-col justify-center text-white rounded">
             <a href="https://discord.gg/FFQKX7RSct">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://share.cubecloud.ca/299729731237052417/SUljtf.jpg" />
+                            <img className="rounded-full h-32 w-32" src="/partnerimg/Nifty-Servers.jpg" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Nifty Servers</h1>        
                                 <div className="flex items-center mt-1">
